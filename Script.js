6b69f1055a20d3d42ce505104cd1e051d12215a5
MahmoudProg <m.daoud.01001101@gmail.com>
2018-09-02 22:50:32 +0200

    V0.2

--- Script.js
+++ Script.js
@@ -1,10 +1,13 @@
 
-if (screen.width > 600) { 
+if (screen.width > 500) { 
         document.body.style.background = 'rgb(120,10,10)'; 
+}else{
+
+    document.body.style.background='rgb(150,10,10)';
     } 
 
 function Btn_Alert_1(){ 
-    document.body.style.backgroundColor = 'rgb(100,200,32)';
+    document.body.style.background = 'rgb(100,200,32)';
     alert("بص بقا الصفحة هتقلبلك اخضر لموني ايه عجب ^_^");
 }
 function Btn_Alert_2(){
