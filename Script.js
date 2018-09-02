e338303fb81c4b68e69d9eed0a5a0cf4bebce958
MahmoudProg <m.daoud.01001101@gmail.com>
2018-09-02 22:47:56 +0200

    V0.1

---
+++ Script.js
@@ -0,0 +1,17 @@
+
+if (screen.width > 600) { 
+        document.body.style.background = 'rgb(120,10,10)'; 
+    } 
+
+function Btn_Alert_1(){ 
+    document.body.style.backgroundColor = 'rgb(100,200,32)';
+    alert("بص بقا الصفحة هتقلبلك اخضر لموني ايه عجب ^_^");
+}
+function Btn_Alert_2(){
+    document.body.style.background = 'rgb(10,90,232)';
+    alert("استنا هخليلك الصفحة زرقا علي دماغك دلوقتي");
+}
+function Btn_Alert_3(){
+    document.body.style.background = 'rgb(255,255,255)';
+    alert("ريح دماغك مش حاطت حاجة هنا");
+}
